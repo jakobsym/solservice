837c686aa5a3b0fbb47bf21f470e8b3736fb38eb
jakobsym <jakob.symchych@my.utsa.edu>
2024-03-05 14:10:57 -0600

    setup mysql db connection

--- application/app.go
+++ application/app.go
@@ -2,10 +2,14 @@ package application
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type App struct {
@@ -20,13 +24,39 @@ func New() *App {
 }
 
 func (a *App) Start(ctx context.Context) error {
+	err := godotenv.Load()
+
+	if err != nil {
+		return fmt.Errorf("error loading data from .env file: %w", err)
+	}
+
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    os.Getenv("DBNET"),
+		Addr:   os.Getenv("DBADDR"),
+		DBName: os.Getenv("DBNAME"),
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Db connection established.")
+
 	server := &http.Server{
 		Addr:    ":3000",
 		Handler: a.router,
 	}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("failed to start server: %w", err)
 	}
+
 	return nil
 }
