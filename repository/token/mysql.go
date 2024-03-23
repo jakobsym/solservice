d20270707f02df2bb23e733084b8f18047995e46
jakobsym <jakob.symchych@my.utsa.edu>
2024-03-23 12:46:23 -0500

    test commit

--- repository/token/mysql.go
+++ repository/token/mysql.go
@@ -7,22 +7,11 @@ import (
 	"github.com/jakobsym/solservice/model"
 )
 
-// database of all tokens I want to add using the Token struct as the attributes
-
-/*
-	type Token struct {
-		CoinAddress   string  `json:"id"`
-		MintSymbol    string  `json:"mintSymbol"`
-		VsTokenSymbol string  `json:"VsTokenSymbol,omitempty"`
-		Price         float64 `json:"price,omitempty"`
-	}
-*/
 type MySqlRepo struct {
 	DB *sql.DB
 }
 
 // insert token
-// inserts CA + Ticker, returning the ticker to user letting them know what was inserted.
 func (ms *MySqlRepo) InsertToken(token *model.Token) error {
 	_, err := ms.DB.Exec("INSERT INTO Token(MintSymbol, CoinAddress) VALUES(?,?)", token.MintSymbol, token.CoinAddress)
 	if err != nil {
@@ -31,8 +20,45 @@ func (ms *MySqlRepo) InsertToken(token *model.Token) error {
 	return nil
 }
 
-//res, err := db.Exec()
-
 // delete token
+func (ms *MySqlRepo) DeleteToken(ca string) error {
+	_, err := ms.DB.Exec("DELETE FROM Token WHERE CoinAddress = ?" + ca)
+	if err != nil {
+		return fmt.Errorf("error inserting token into db: %w", err)
+	}
+	return nil
+}
 
-// search for token
+// search for a token
+func (ms *MySqlRepo) QueryToken(ca string) (model.Token, error) {
+	var token model.Token
+	res, err := ms.DB.Query("SELECT CoinAddress FROM Token WHERE CoinAddress = ?" + ca)
+	if err != nil {
+		return model.Token{}, nil
+	}
+	if err := res.Scan(&token.CoinAddress, &token.MintSymbol); err != nil {
+		return model.Token{}, nil
+	}
+	return token, nil
+}
+
+// search for all tokens
+func (ms *MySqlRepo) QueryAllTokens() []model.Token {
+	var tokens []model.Token
+	rows, err := ms.DB.Query("SELECT * FROM Token.CoinAddress")
+	if err != nil {
+		return []model.Token{}
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var token model.Token
+		if err := rows.Scan(&token.CoinAddress, &token.MintSymbol); err != nil {
+			return nil
+		}
+		tokens = append(tokens, token)
+	}
+	if err := rows.Err(); err != nil {
+		return nil
+	}
+	return tokens
+}
